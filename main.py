f9d115e6fbdad7f1ae93f05b57897956915ceff2
Adina <adinalarisa1234@gmail.com>
2025-11-03 15:36:09 +0200

    tema2

--- main.py
+++ main.py
@@ -1,2 +1,41 @@
-print("salut")
-print("salut,git")
+#structuri decizionale:if ,elif,else
+#if conditie : bloc de cod pentru True
+#elif alta_conditie :bloc de cod pt alta conditie True
+#else:bloc cod pt Flase
+numar=10
+if numar > 0:
+    print("Numar este pozitiv")
+elif numar < 0:
+    print("Numar este negativ")
+else:
+    print("Numar este zero")
+#se da o nota sa se afiseze daca a promovat sau nu
+nota= 4
+if nota >= 5
+    print("Promovat")
+else:
+    print("Nepromovat")
+    # se da o nota sa se afiseze daca e par sau impar
+#un numar este par daca restul impartiri la 2 e 0
+#modulu:
+
+
+
+
+
+
+#structuri repetitive:for ,while
+#while:folosit cand nu stim dinainte de cate ori trebuie sa repetam un bloc de cod
+#sintaxa
+#while conditie:
+
+while 10 > 5
+    print("10 este mai mare decat 5 ")
+    numar=5
+    while numar > 0:
+     print(numar)
+     numar-=1          #echivalent cu numar =numar - 1
+#cate cifre are un numar
+#numar=10002
+
+
